09fe3a5e55bfd40121aff16451b748a011ed4deb
wedev <172839087+hellowedev@users.noreply.github.com>
2025-07-24 02:18:04 +0530

    Fix 500 issue on signup page

--- communicate/settings.py
+++ communicate/settings.py
@@ -151,11 +151,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-if DEBUG:
-    # Where to find your static file
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-else:
+if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
